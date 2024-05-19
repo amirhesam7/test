e8736477820f434e92557a170d9e12659f008939
unknown <azinazi2244@gmail.com>
2024-05-19 18:02:14 +0330

    mojtaba w3orkout

--- test.py
+++ test.py
@@ -5,7 +5,7 @@ import random as rn
 win = tr.Screen()
 pen = tr.Turtle()
 pen.pensize(1)
-color = ['1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F']
+
 pen.speed(0)
 h = 1
 n = 70
